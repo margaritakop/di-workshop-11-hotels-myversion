b0f74017917437f5fd162334ccc4bbd4fd05f00d
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 12:06:11 +0000

    add functionality comments for review to test

--- test/review.tests.js
+++ test/review.tests.js
@@ -5,4 +5,16 @@ var Hotel = require('../models/review')
 
 describe('Review', function() {
 
+    //var review1 = new Review(5, "Excellent hotel, very clean", "2018-12-17")
+    //review1.rating //=> 3
+    //review1.text //=> "Excellent hotel, very clean"
+    //review1.date //=> A javascript Date object for 2018-12-17
+    //review1.ratingAsStars() //=> "⭐️⭐️⭐️⭐️⭐"
+
+    //var review2 = new Review(1, "Terrible hotel, smelled of mice", "2018-01-01")
+    //review2.rating //=> 1
+    //review2.text //=> "Terrible hotel, smelled of mice"
+    //review2.date //=> A javascript Date object for 2018-01-01
+    //review2.ratingAsStars() //=> "⭐️"
+
 });
