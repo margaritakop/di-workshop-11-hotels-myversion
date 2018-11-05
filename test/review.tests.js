f17a55779ea25ea9e9a8e81c50c25697f789e1d9
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 14:20:51 +0000

    add test for Review intenstiation rating and text

--- test/review.tests.js
+++ test/review.tests.js
@@ -1,13 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-var Hotel = require('../models/review')
+var Review = require('../models/review')
 
 describe('Review', function() {
 
-    //var review1 = new Review(5, "Excellent hotel, very clean", "2018-12-17")
-    //review1.rating //=> 3
-    //review1.text //=> "Excellent hotel, very clean"
+    let review1 = new Review (5, "Excellent hotel, very clean", "2018-12-17")
+
+    expect(review1.rating).equal(5)
+    expect(review1.text).equal("Excellent hotel, very clean")
+
     //review1.date //=> A javascript Date object for 2018-12-17
     //review1.ratingAsStars() //=> "⭐️⭐️⭐️⭐️⭐"
 
