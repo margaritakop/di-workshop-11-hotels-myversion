8d0ef1c2de95d1ffe8b40119fb37666972891fcc
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 12:03:15 +0000

    add requirements for hotel as comments in the test

--- test/hotel.tests.js
+++ test/hotel.tests.js
@@ -5,4 +5,15 @@ var Hotel = require('../models/hotel')
 
 describe('Hotel', function() {
 
+    //var hotel = new Hotel("Hilton Metropole", "London")
+    //hotel.name //=> "Hilton Metropole"
+    //hotel.city //=> "London"
+    //hotel.reviews //=> []
+    //hotel.reviewCount() //=> 0
+    //hotel.rating() //=> 0
+    //hotel.ratingAsStars() //=> ''
+    
+    //hotel.urlSlug() //=> "hilton_metropole_london"
+
+
 });
