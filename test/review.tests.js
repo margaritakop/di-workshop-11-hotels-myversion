a6d41719ce89afa2ba02394b3c9c27a93c99fab1
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 14:21:49 +0000

    fix instantiate review test

--- test/review.tests.js
+++ test/review.tests.js
@@ -7,8 +7,10 @@ describe('Review', function() {
 
     let review1 = new Review (5, "Excellent hotel, very clean", "2018-12-17")
 
+    it ('should instanciate', ()=>{
     expect(review1.rating).equal(5)
     expect(review1.text).equal("Excellent hotel, very clean")
+    })
 
     //review1.date //=> A javascript Date object for 2018-12-17
     //review1.ratingAsStars() //=> "⭐️⭐️⭐️⭐️⭐"
