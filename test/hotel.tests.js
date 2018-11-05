ea95bd0d323ada0d435186f02ff66eb06ca69adf
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 14:14:00 +0000

    refactor hotel test object

--- test/hotel.tests.js
+++ test/hotel.tests.js
@@ -5,40 +5,36 @@ var Hotel = require('../models/hotel')
 
 describe('Hotel', function() {
 
-    //TODO refactor the creation of the hotel object over and over again
+    let hotel = new Hotel("Hilton Metropole", "London")
+
     it ('should instanciate properly', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London")
         expect(hotel.name).equal("Hilton Metropole")
         expect(hotel.city).equal("London")
         expect(hotel.reviews).deep.equal([])
     })
 
     it ('should count reviews', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London")
         expect(hotel.reviewCount()).equal(0)
     })
 
     it ('should return 0 rating when no reviews', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London")
         expect(hotel.rating()).equal(0)
 
         //TODO: Implement a second test when array is not empty
     })
 
     it ('should expect no stars when there are no reviews', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London UK")
         expect(hotel.ratingAsStars()).equal('')
 
         //TODO: Implement a second test when array is not empty
     })
 
     it ('should return an _formatted string of the hotel', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London")
         expect(hotel.urlSlug()).equal("hilton_metropole_london")
     })
 
     it ('should return an _formatted string of the hotel with multiple spaces', ()=>{
-        var hotel = new Hotel("Hilton Metropole", "London UK")
+        let hotel = new Hotel("Hilton Metropole", "London UK")
         expect(hotel.urlSlug()).equal("hilton_metropole_london_uk")
     })
 
