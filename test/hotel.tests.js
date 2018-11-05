076bb304037d93341d5b9d56992e6378102a6382
margaritakop <margarita.kopniczky@gmail.com>
2018-11-05 15:53:11 +0000

    refactor tests

--- test/hotel.tests.js
+++ test/hotel.tests.js
@@ -5,54 +5,50 @@ var Hotel = require('../models/hotel')
 var Review = require('../models/review')
 
 describe('Hotel', function() {
-
-    let hotel = new Hotel("Hilton Metropole", "London")
+    
+    //the first hotel1 without reviews
+    let hotel1 = new Hotel("Hilton Metropole", "London")
+    
+    //make and add some reviews to the second hotel1
+    let hotel2 = new Hotel("Hilton on the Hill", "Scotland UK")
+    let review1 = new Review (5, "Excellent hotel1, very clean", "2018-12-17")
+    let review2 = new Review(1, "Terrible hotel1, smelled of mice", "2018-01-01")
+    hotel2.addReview(review1)
+    hotel2.addReview(review2)
 
     it ('should instanciate properly', ()=>{
-        expect(hotel.name).equal("Hilton Metropole")
-        expect(hotel.city).equal("London")
-        expect(hotel.reviews).deep.equal([])
+        expect(hotel1.name).equal("Hilton Metropole")
+        expect(hotel1.city).equal("London")
+        expect(hotel1.reviews).deep.equal([])
     })
 
     it ('should count reviews', ()=>{
-        expect(hotel.reviewCount()).equal(0)
+        expect(hotel1.reviewCount()).equal(0)
     })
 
     it ('should return 0 rating when no reviews', ()=>{
-        expect(hotel.rating()).equal(0)
-
-        //TODO: Implement a second test when array is not empty
+        expect(hotel1.rating()).equal(0)
+    })
+    it('should take an average of all ratings', () => {
+        expect(hotel2.rating()).equal(3)
     })
 
     it ('should expect no stars when there are no reviews', ()=>{
-        expect(hotel.ratingAsStars()).equal('')
+        expect(hotel1.ratingAsStars()).equal('')
         expect(hotel2.ratingAsStars()).equal("⭐️⭐️⭐️")
     })
 
-    it ('should return an _formatted string of the hotel', ()=>{
-        expect(hotel.urlSlug()).equal("hilton_metropole_london")
+    it ('should return an _formatted string of the hotel1', ()=>{
+        expect(hotel1.urlSlug()).equal("hilton_metropole_london")
     })
 
-    it ('should return an _formatted string of the hotel with multiple spaces', ()=>{
-        let hotel = new Hotel("Hilton Metropole", "London UK")
-        expect(hotel.urlSlug()).equal("hilton_metropole_london_uk")
+    it ('should return an _formatted string of the hotel1 with multiple spaces', ()=>{
+        let hotel1 = new Hotel("Hilton Metropole", "London UK")
+        expect(hotel1.urlSlug()).equal("hilton_metropole_london_uk")
     })
-    
-    //make and add some reviews
-    let hotel2 = new Hotel("Hilton on the Hill", "Scotland UK")
-    let review1 = new Review (5, "Excellent hotel, very clean", "2018-12-17")
-    let review2 = new Review(1, "Terrible hotel, smelled of mice", "2018-01-01")
-    hotel2.addReview(review1)
-    hotel2.addReview(review2)
 
     it('should record reviews', ()=>{
         expect(hotel2.reviews).deep.equal([review1, review2])
         expect(hotel2.reviewCount()).equal(2)
     })
-
-    it('should take an average of all ratings', () => {
-        expect(hotel2.rating()).equal(3)
-    })
-    
-    //hotel.ratingAsStars() //=> "⭐️⭐️⭐️"
 });
